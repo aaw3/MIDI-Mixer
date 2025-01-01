70d07afe3f9e2c47e6db70ef564650a6546010a8
Allen Wolf <63997543+aaw3@users.noreply.github.com>
2024-12-31 22:34:21 -0600

    Add modules folder for processing different plugins like easyeffects processor

---
+++ modules/base.py
@@ -0,0 +1,12 @@
+class KnobFunction:
+    def __init__(self):
+        pass
+
+
+class FaderFunction:
+    def __init__(self):
+        pass
+
+class ButtonFunction:
+    def __init__(self):
+        pass
